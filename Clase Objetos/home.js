3b83e2597476d2ff9d0541341bf7a5950c680df9
Roberto <sexyskavenlover@gmail.com>
2021-06-26 15:41:06 -0300

    agregado comments

--- Clase Objetos/home.js
+++ Clase Objetos/home.js
@@ -1,18 +1,21 @@
 
+//objeto inicial que modificará el prompt
 var paciente ={
 nombre: "",
 edad: "",
 peso: "",
 altura: "",
 }
+//objeto inicial que modificará el prompt
 
+//Modificadores para el objeto que guardará los datos
 var bucle = 0;
 
 var registrados = {
 }
+//Modificadores para el objeto que guardará los datos guardados
 
-
-
+//Boton para pedir datos
 function begin(){
     paciente.nombre = prompt("Ingrese Nombre");
     paciente.edad = parseFloat(prompt("Ingrese Edad"));
@@ -20,8 +23,9 @@ function begin(){
     paciente.altura = parseFloat(prompt("Ingrese Altura"));
     alert("Si los datos son correctos por favor presione en 'Registrar'");
 }
+//Boton para pedir datos
 
-
+//Boton para guardar los datos
 function registrar(){
     if(paciente.nombre == ""){
         alert("Faltan Datos, por favor presione 'Comenzar'")
@@ -31,11 +35,22 @@ registrados[bucle]  = paciente;
 paciente = { nombre:"" };
     }
 }
+//Boton para guardar los datos
 
+
+//Boton para mostrar los datos guardados en consola
 function mostrar(){
     alert("Por favor chequee la consola");
     console.log(registrados);
 }
+//Boton para mostrar los datos guardados en consola
+
+
+
+
+
+
+//Moldeador que no me funcionó :( ↓
 
 /*
 function moldeador(pNombre, pEdad, pPeso, pAltura){
@@ -49,7 +64,7 @@ function moldeador(pNombre, pEdad, pPeso, pAltura){
 var paciente20 = new moldeador(paciente.nombre, paciente.edad, paciente.peso, paciente.altura);
 */
 
-
+//Moldeador que no me funcionó :( ↑
 
 
 
