dbbb777fed25d149a41c70b147be3cd6a296bed4
Roberto <sexyskavenlover@gmail.com>
2021-07-15 16:54:09 -0300

    modificaciones

--- Final1/home.js
+++ Final1/home.js
@@ -1,11 +1,12 @@
+let registrados = JSON.parse(localStorage.Registra2);
+var vturno = 1;
+const mShow = document.querySelector('#listado');
 
-const registrados = [];
-//const fecha = new Date(2020, registrados.mes, registrados.dia, registrados.hora);
-const mShow = document.querySelector('.listado');
 
 //Boton para pedir datos
 function begin() {
     const paciente = {};
+    paciente.turno = vturno++;
     paciente.nombre = prompt("Ingrese Nombre");
     paciente.edad = parseFloat(prompt("Ingrese Edad"));
     paciente.peso = parseFloat(prompt("Ingrese Peso"));
@@ -22,7 +23,8 @@ function registrar(nuevoPaciente) {
     if (nuevoPaciente.nombre == "") {
         return alert("Faltan Datos, por favor presione 'Comenzar'");
     }
-    registrados.push(nuevoPaciente);
+    localStorage.setItem(`Registra2`, JSON.stringify(nuevoPaciente));
+   // registrados.push(nuevoPaciente);
 };
 //Funcion de Registro
 
@@ -30,10 +32,21 @@ function registrar(nuevoPaciente) {
 //Funcion para Mostrar
 function mostrar() {
     mShow.innerHTML = "Pacientes ";
-    const mListado = registrados => mShow.innerHTML +=`<li> 2020/${registrados.mes}/${registrados.dia} A las ${registrados.hora} turno de ${registrados.nombre} </li>`;
+    const mListado = registrados => mShow.innerHTML +=`<li>Turno número: ${registrados.turno}  2020/${registrados.mes}/${registrados.dia} A las ${registrados.hora} turno de ${registrados.nombre} </li>`;
     registrados.forEach(mListado);
-    
     alert("Por favor chequee la consola");
     console.log(registrados);
 }
 //Funcion para Mostrar
+
+
+function remover(){
+    let eliminar = document.querySelector('#limpiar').value;
+    if (eliminar > 1){
+        eliminar--;
+        delete registrados[eliminar];
+    } else {registrados.shift()};
+   
+    mostrar();
+
+}
